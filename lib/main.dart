3aae541948f429d343a6344a7b674cae311739a8
chanaka <chanaka@cybergate.lk>
2023-10-19 04:08:01 +0530

    Implemented a foreground service

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,25 @@
 // ignore_for_file: must_be_immutable
 
+import 'dart:async';
+
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_awesome_notifications_in_background/notification.dart';
 
-void main() {
+import 'dart:io';
+import 'dart:ui';
+
+// import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+import 'package:flutter_background_service_android/flutter_background_service_android.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  /// Start the background service
+  await initializeService();
+
   /// Initialization
   AwesomeNotifications().initialize(
     // set the icon to null if you want to use the default app icon
@@ -197,4 +212,163 @@ class _MyNotificationPageState extends State<MyNotificationPage> {
       ),
     );
   }
+}
+
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+
+  /// OPTIONAL, using custom notification channel id
+  const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'my_foreground', // id
+    'MY FOREGROUND SERVICE', // title
+    description:
+        'This channel is used for important notifications.', // description
+    importance: Importance.low, // importance must be at low or higher level
+  );
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  if (Platform.isIOS || Platform.isAndroid) {
+    await flutterLocalNotificationsPlugin.initialize(
+      const InitializationSettings(
+        iOS: DarwinInitializationSettings(),
+        android: AndroidInitializationSettings('ic_bg_service_small'),
+      ),
+    );
+  }
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      // this will be executed when app is in foreground or background in separated isolate
+      onStart: onStart,
+
+      // auto start service
+      autoStart: true,
+      isForegroundMode: true,
+
+      notificationChannelId: 'my_foreground',
+      initialNotificationTitle: 'AWESOME SERVICE',
+      initialNotificationContent: 'Initializing',
+      foregroundServiceNotificationId: 888,
+    ),
+    iosConfiguration: IosConfiguration(
+      // auto start service
+      autoStart: true,
+
+      // this will be executed when app is in foreground in separated isolate
+      onForeground: onStart,
+
+      // you have to enable background fetch capability on xcode project
+      onBackground: onIosBackground,
+    ),
+  );
+
+  service.startService();
+}
+
+/// Handling the iOS bacground task
+@pragma('vm:entry-point')
+Future<bool> onIosBackground(ServiceInstance service) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  DartPluginRegistrant.ensureInitialized();
+
+  // TODO: code here
+
+  return true;
+}
+
+/// Migrations
+/// * sendData() renamed to invoke(String method) <=== send data from background service to the app
+/// * onDataReceived() renamed to on(String method) <=== receive data from app to the background service
+
+/// We have passed a service instance here
+/// Therefore no need to manually recreate it
+@pragma('vm:entry-point')
+void onStart(ServiceInstance service) async {
+  /// Ensure initialized
+  DartPluginRegistrant.ensureInitialized();
+
+  /// My instances
+  // final audioPlayer = AudioPlayer();
+
+  // /// My variables
+  // String audioUrl =
+  //     "https://commondatastorage.googleapis.com/codeskulptor-assets/Collision8-Bit.ogg";
+  // int count = 0;
+
+  /// If you need to show a custom notification (OPTIONAL)
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  if (service is AndroidServiceInstance) {
+    service.on('setAsForeground').listen((event) {
+      service.setAsForegroundService();
+    });
+
+    service.on('setAsBackground').listen((event) {
+      service.setAsBackgroundService();
+    });
+  }
+
+  /// Event to stop the background service (EVENT BUS/ MESSAGE PASSING)
+  service.on('stopService').listen((event) {
+    service.stopSelf();
+  });
+
+  // audioPlayer.play(UrlSource(audioUrl));
+
+  // /// My methods
+  // audioPlayer.onPlayerStateChanged.listen((event) {
+  //   if (event == PlayerState.completed) {
+  //     Map<String, dynamic> dataToSend = {
+  //       'count': count++,
+  //     };
+
+  //     /// Send data back to the UI (EVENT BUS/ MESSAGE PASSING)
+  //     service.invoke('update', dataToSend);
+
+  //     /// Play audio
+  //     audioPlayer.play(UrlSource(audioUrl));
+
+  //     print(count);
+  //   }
+  // });
+
+  // bring to foreground
+  Timer.periodic(const Duration(seconds: 1), (timer) async {
+    if (service is AndroidServiceInstance) {
+      if (await service.isForegroundService()) {
+        /// OPTIONAL for use custom notification
+        /// the notification id must be equals with AndroidConfiguration when you call configure() method.
+        flutterLocalNotificationsPlugin.show(
+          888,
+          'COOL SERVICE',
+          'Awesome ${DateTime.now()}',
+          const NotificationDetails(
+            android: AndroidNotificationDetails(
+              'my_foreground',
+              'MY FOREGROUND SERVICE',
+              icon: 'ic_bg_service_small',
+              ongoing: true,
+            ),
+          ),
+        );
+
+        // if you don't using custom notification, uncomment this
+        service.setForegroundNotificationInfo(
+          title: "My App Service",
+          content: "Updated at ${DateTime.now()}",
+        );
+      }
+    }
+
+    /// Logging that the bacground service is running
+    print('Background service running: ${DateTime.now()}');
+  });
 }
